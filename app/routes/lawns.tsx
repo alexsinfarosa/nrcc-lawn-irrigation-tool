ae334a477a567dd8f735796b8b36c09691134d78
Alex Sinfarosa <alexsinfarosa@users.noreply.github.com>
2022-04-22 18:41:56 +0200

    updates

--- app/routes/lawns.tsx
+++ app/routes/lawns.tsx
@@ -1,12 +1,18 @@
 import { Fragment, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
-import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import {
+  LogoutIcon,
+  MenuIcon,
+  PlusIcon,
+  XIcon,
+} from "@heroicons/react/outline";
 
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Link, NavLink, Outlet, useLoaderData } from "@remix-run/react";
+import { Form, Link, NavLink, Outlet, useLoaderData } from "@remix-run/react";
 
 import { requireUserId } from "~/session.server";
+import { useUser } from "~/utils";
 import { getLawnListItems } from "~/models/lawn.server";
 import nyWaterLogo from "../images/ny-water-logo.svg";
 
@@ -23,6 +29,7 @@ export const loader: LoaderFunction = async ({ request }) => {
 export default function Example() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const data = useLoaderData() as LoaderData;
+  const user = useUser();
 
   return (
     <>
@@ -89,7 +96,11 @@ export default function Example() {
                     onClick={() => setSidebarOpen(false)}
                     className="inline-flex items-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
                   >
-                    + New Lawn
+                    <PlusIcon
+                      className="-ml-1 mr-2 h-5 w-5"
+                      aria-hidden="true"
+                    />
+                    New Lawn
                   </Link>
                 </div>
 
@@ -132,6 +143,20 @@ export default function Example() {
                     </>
                   )}
                 </nav>
+              </div>
+              <div className="flex flex-shrink-0 justify-center border-t border-gray-200 p-4">
+                <Form action="/logout" method="post">
+                  <button
+                    type="submit"
+                    className="inline-flex items-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                  >
+                    <LogoutIcon
+                      className="-ml-1 mr-2 h-5 w-5"
+                      aria-hidden="true"
+                    />
+                    Logout
+                  </button>
+                </Form>
               </div>
             </div>
           </Transition.Child>
@@ -200,6 +225,21 @@ export default function Example() {
                 </>
               )}
             </nav>
+          </div>
+          <div className="flex flex-shrink-0 flex-col border-t border-gray-200 p-4">
+            <Form
+              action="/logout"
+              method="post"
+              className="flex justify-center"
+            >
+              <button
+                type="submit"
+                className="inline-flex items-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+              >
+                <LogoutIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+                Logout
+              </button>
+            </Form>
           </div>
         </div>
       </div>
